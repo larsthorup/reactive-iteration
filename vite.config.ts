0a9e7c1294e7bdc611526e299736b3eee6834f2d
Lars Thorup <lars@zealake.com>
2023-06-30 16:49:19 +0200

    publish coverage

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['lcov', 'text', 'html'],
+    },
     environment: 'jsdom',
     setupFiles: ['src/vitest.setup.ts']
   },
